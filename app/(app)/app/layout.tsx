588b7dfdb631416cd1f57bc3e63114d475640be9
Itay Sidis <itaysidis@gmail.com>
2024-09-20 14:46:16 +0300

    app footer

--- app/(app)/app/layout.tsx
+++ app/(app)/app/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: PropsWithChildren) {
   return (
     <>
       <BackgroundPattern />
-      <div className="max-w-7xl mx-auto px-4">
+      <div className="flex flex-col max-w-7xl mx-auto px-4 min-h-screen">
         <AppHeader />
         {children}
         <AppFooter />
--- app/components/AppFooter.tsx
+++ app/components/AppFooter.tsx
@@ -1,3 +1,11 @@
 export default function AppFooter() {
-  return <div>AppFooter</div>;
+  return (
+    <footer className="mt-auto flex justify-between items-center border-t border-black/5 py-5">
+      <p>© 2024</p>
+      <div>
+        <p>Privacy Policy</p>
+        <p>Terms of Service</p>
+      </div>
+    </footer>
+  );
 }
