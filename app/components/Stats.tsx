d2491bd4a77f2bf931f0796400b610baabdf7fcd
Itay Sidis <itaysidis@gmail.com>
2024-09-23 07:49:06 +0300

    stats component connected to state

--- app/components/Stats.tsx
+++ app/components/Stats.tsx
@@ -1,7 +1,12 @@
+"use client";
+
+import { usePetsSelector } from "../(app)/app/stores/PetsProvider";
+
 export default function Stats() {
+  const pets = usePetsSelector();
   return (
     <section className="text-center">
-      <p className="text-2xl font-bold leading-6">2</p>
+      <p className="text-2xl font-bold leading-6">{pets.length}</p>
       <p className="opacity-80">current guests</p>
     </section>
   );
